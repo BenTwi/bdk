01dc6823994f33ac214fdfcbe1988d943f60cb1c
Skytro <skytro@skykopf.de>
2024-11-03 16:47:24 +0100

    Added domains

--- index.js
+++ index.js
@@ -14,11 +14,22 @@
 
 let socket;
 let BackendWasConnected = false;
+let domains;
 
 
 function loadDependencys(){
 
     const dependencys = ['connector', 'utils', 'logger', 'mappings', 'runtimes']
+
+        // Replace ./data.json with your JSON feed
+    fetch('https://bentwi.skykopf.com/dev-kit/config/domains.json').then(response => {
+        return response.json();
+    }).then(data => {
+        log("log", "Successfully loaded domains", "MAIN")
+    }).catch(err => {
+        // Do something for an error here
+    });
+
 
     dependencys.forEach(dependency => {
 
@@ -26,6 +37,8 @@ function loadDependencys(){
         depSrc.src = `https://bentwi.skykopf.com/dev-kit/dependencys/${dependency}.js`;
 
         document.body.appendChild(depSrc)
+
+        log("log", `Loaded dependency "${dependency}"`, "MAIN")
 
     })
 
