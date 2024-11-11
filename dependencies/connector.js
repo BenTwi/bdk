256e6bf9ed2e39dc1c88340dcdc0015de5821b41
Skytro <skytro@skykopf.de>
2024-11-11 19:39:00 +0100

    Added Auto Ping/Pong

--- dependencies/connector.js
+++ dependencies/connector.js
@@ -14,7 +14,6 @@ BENTWI.connector = {
         }
     },
     reconnect: (after) => {
-
         if(!BENTWI.connector.socket){
             log('warn', `Can't use reconnect when no session is active`, 'CONNECTOR')
             return;
@@ -27,12 +26,8 @@ BENTWI.connector = {
         BENTWI.connector.socket = null;
     
         setTimeout(() => {
-    
             BENTWI.connector.connect(BENTWI.domains.socket)
-    
         }, after)
-    
-    
     },
     disconnect: () => {
         BENTWI.connector.socket.close()
@@ -42,9 +37,8 @@ BENTWI.connector = {
     socket: null,
 
     socketController: (socket) => {
-
         if(!socket){
-            log("error", "socektController didn't receive an socket object", "CONNECTOR")
+            log("error", "socketController didn't receive a socket object", "CONNECTOR")
             return;
         }
 
@@ -54,10 +48,10 @@ BENTWI.connector = {
 
         socket.addEventListener("close", () => {
             log("warn", "BenTwi's connection to the backend was closed", "CONNECTOR")
-                if(BENTWI.config.preferences.auto_reconnect_on_connection_loss){
-                    log("warn", "BenTwi will try to reconnect!")
-                    BENTWI.connector.reconnect(5)
-                }
+            if(BENTWI.config.preferences.auto_reconnect_on_connection_loss){
+                log("warn", "BenTwi will try to reconnect!")
+                BENTWI.connector.reconnect(5)
+            }
         })
 
         socket.addEventListener("error", (err) => {
@@ -65,16 +59,47 @@ BENTWI.connector = {
         })
 
         socket.addEventListener("message", (message) => {
-            BENTWI.connector.messageHandler(message.data)            
+            BENTWI.connector.messageHandler(message.data)
         })
-
     },
 
     messageHandler: (message) => {
-
-        log("warn", "BenTwi received an message from the backend: ", "DEBUG")
+        log("warn", "BenTwi received a message from the backend: ", "DEBUG")
         console.log(message)
 
-    }
+        // Parse message as JSON
+        let parsedMessage;
+        try {
+            parsedMessage = JSON.parse(message);
+        } catch (error) {
+            log("error", "Failed to parse message: " + message, "CONNECTOR")
+            return;
+        }
 
+        // Use switch statement to handle different message types by ID
+        switch (parsedMessage.ID) {
+            case "OB2OF_PING":
+                // Respond with PONG message
+                BENTWI.connector.sendMessage({
+                    ID: "OF2OB_PONG"
+                });
+                log("info", "Responded with OF2OB_PONG", "CONNECTOR")
+                break;
+            
+            // Add more cases here for handling other messages as needed
+
+            default:
+                log("warn", "Received an unhandled message ID: " + parsedMessage.ID, "CONNECTOR")
+                break;
+        }
+    },
+
+    sendMessage: (messageObj) => {
+        if (BENTWI.connector.socket && BENTWI.connector.socket.readyState === WebSocket.OPEN) {
+            BENTWI.connector.socket.send(JSON.stringify(messageObj));
+            log("info", "Sent message: " + JSON.stringify(messageObj), "CONNECTOR");
+        } else {
+            log("warn", "Unable to send message; socket is not open", "CONNECTOR");
+        }
+    }
 }
