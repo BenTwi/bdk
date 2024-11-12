f7d22e955c94d9a65ce8cbdb06f52da5431f6463
Skytro <skytro@skykopf.de>
2024-11-12 17:54:51 +0100

    Changend BENTWI.config.set() behaviour

--- dependencies/config.js
+++ dependencies/config.js
@@ -1,10 +1,13 @@
 BENTWI.config.set = (config) => {
 
-    log("log", `Set new config with artifact: ${config.overlay.artifact}`)
-
     if(config.connection.token != "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" && config.overlay.artifact != "com.myname.overlays.firstoverlay" && config.overlay.name != "My first overlay"){
-        BENTWI.config = config;
-        setTimeout(() => { updateDocumentTitle(`${BENTWI.config.overlay.name} | BenTwi`) }, 500)
+        BENTWI.config = Object.assign({}, BENTWI.config, config);
+        
+        setTimeout(() => { 
+        console.log(BENTWI.config)
+        
+        log("log", `Set new config with artifact: ${config.overlay.artifact}`)
+        updateDocumentTitle(`${BENTWI.config.overlay.name} | BenTwi`) }, 500)
     } else {
         log("error", "Config wasn't able to be set because either token, artifact or name was not set!", "SESSION")
         return;
