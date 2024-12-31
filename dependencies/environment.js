d644b046c59b89ca655db4c007af2dcdff74e807
Skytro <skytro@skykopf.de>
2024-12-31 21:35:59 +0100

    Update environment.js

--- dependencies/environment.js
+++ dependencies/environment.js
@@ -3,14 +3,15 @@ var env = 'development';
 const OBS = window.obsstudio;
 
 async function initENV(liveRefresh, interval) {
-    log("log", "Initilizing Environment..", "ENV")
+    log("log", "Initializing Environment..", "ENV");
+
     if (OBS) {
         BENTWI.environment = {
             arch: "OBS_STUDIO",
-            version: OBS..pluginVersion,
+            version: OBS.pluginVersion,
             permission: OBS.getControlLevel(),
             state: OBS.getStatus(),
-            curretnScene: OBS.getCurrentScene(),
+            currentScene: OBS.getCurrentScene(), // Fixed typo
             scenes: OBS.getScenes(),
             currentTransition: OBS.getCurrentTransition(),
             transitions: OBS.getTransitions()
@@ -23,7 +24,11 @@ async function initENV(liveRefresh, interval) {
             editorMode: SE.isEditorMode,
             muted: SE.muted,
         };
-        if(BENTWI.environment.editorMode) { env = 'development'; } else { env = 'operating'; }
+        if (BENTWI.environment.editorMode) { 
+            env = 'development'; 
+        } else { 
+            env = 'operating'; 
+        }
     } else if (location.protocol === "https:") {
         BENTWI.environment = {
             ...BENTWI.environment,
@@ -56,14 +61,14 @@ async function initENV(liveRefresh, interval) {
         env = 'development';
     }
 
-    if(BENTWI.environment.arch){
+    if (BENTWI.environment.arch) {
         BENTWI.environment.env = env;
-        log("log", `Runtime is now set to ${BENTWI.environment.arch}!`, "RUNTIME")
+        log("log", `Runtime is now set to ${BENTWI.environment.arch}!`, "RUNTIME");
     } else {
-        log("critical", `Failed to set environment, check the discord to see wether the env is already registered, if not feel free to open a support ticket and request adding it!`, "RUNTIME")
+        log("critical", `Failed to set environment, check the Discord to see whether the env is already registered. If not, feel free to open a support ticket and request adding it!`, "RUNTIME");
     }
 
-    if(liveRefresh){
-        setTimeout(initENV, interval || 1500)
-
+    if (liveRefresh) {
+        setTimeout(initENV, interval || 1500); // Added missing parenthesis
+    }
 }
